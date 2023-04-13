b9d0887ad9c42f9323a46a8143d4f0aeecfbc90d
Shardul Singh Negi <44360173+negimags@users.noreply.github.com>
2023-04-13 10:24:41 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,18 +12,9 @@ var initInfo = function(){
     from Northeastern University.  I have a keen intrest in building things and I believe the hard engineering \
     problems are often intrinsically fun to tackle.  \
     I'm looking for a position where I can solve real customer problems with a business justification, where\
-    I can colloborate, reiterate and also learn in the process about system design and architecture building for business applications.";
+    I can collaborate, reiterate and also learn in the process about system design and architecture building for business applications.";
 
     $("#about-text").append( aboutMe );
 
 }
 
-/*
-    PROJECTS:
-
-    1. Steganography
-    2. Graphing
-    3. Twitter Connections
-    4. BST yelp
-    5. Arduino Game
-*/
